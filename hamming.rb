ab8d5d933533d56c94984f2a21cbd0d92c07c904
Stacy Mullins <smullins10@gmail.com>
2017-02-21 19:55:54 -0600

    Initialize repo

---
+++ hamming.rb
@@ -0,0 +1,14 @@
+a=['G','A','G','C','C','T','A','C','T','A','A','C','G','G','G','A','T']
+b=['C','A','T','C','G','T','A','A','T','G','A','C','G','G','C','C','T']
+
+def hamming_difference(a,b)
+  hamming_distance=[]
+  i=0
+  while i <= a.length
+    if a[i] != b[i]
+      difference.push(b[i])
+    end
+    i+=1
+  end
+  hamming_distance.count
+end
