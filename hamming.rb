6158825df0d486c192df0ad0b88601d3d53f05a3
Stacy Mullins <smullins10@gmail.com>
2017-02-21 20:02:47 -0600

    Refactor hamming

--- hamming.rb
+++ hamming.rb
@@ -1,12 +1,14 @@
 a=['G','A','G','C','C','T','A','C','T','A','A','C','G','G','G','A','T']
 b=['C','A','T','C','G','T','A','A','T','G','A','C','G','G','C','C','T']
 
-def hamming_difference(a,b)
+def hamming_difference(strand_one,strand_two)
+  a=strand_one.chars
+  b=strand_two.chars
   hamming_distance=[]
   i=0
   while i <= a.length
     if a[i] != b[i]
-      difference.push(b[i])
+      hamming_distance.push(b[i])
     end
     i+=1
   end
